8af70fb612d82de05b574e62a95bb60054b954cd
maksimuspiter <maksimuspiter@gmail.com>
2023-03-15 16:15:26 +0300

    buttons

--- bot_try1.py
+++ bot_try1.py
@@ -2,6 +2,7 @@ from telebot import types
 from time import sleep
 
 import telebot
+from telebot import types
 
 bot = telebot.TeleBot('5896308515:AAGZwJtgI1OZ_KlMNEzxWbjLT4v2KOlAlVs')
 
@@ -11,9 +12,11 @@ bot = telebot.TeleBot('5896308515:AAGZwJtgI1OZ_KlMNEzxWbjLT4v2KOlAlVs')
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    mess1 = bot.send_message(message.chat.id, '_all_ right')
-    sleep(1)
-    bot.edit_message_text(chat_id=message.chat.id, message_id=mess1.id, text='ass')
+    kb = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
+    btn1 = types.KeyboardButton(text='Button 1')
+    btn2 = types.KeyboardButton(text='Button 2')
+    kb.add(btn1, btn2)
+    bot.send_message(message.chat.id, '1', reply_markup=kb)
 
 
 if __name__ == '__main__':
