f96abfb9c1ba4d0e184f53028b6d175f8aff5f8b
maksimuspiter <maksimuspiter@gmail.com>
2023-03-15 16:09:33 +0300

    change message

--- bot_try1.py
+++ bot_try1.py
@@ -1,4 +1,5 @@
 from telebot import types
+from time import sleep
 
 import telebot
 
@@ -6,38 +7,13 @@ bot = telebot.TeleBot('5896308515:AAGZwJtgI1OZ_KlMNEzxWbjLT4v2KOlAlVs')
 
 
 # @bot.message_handler(commands=['start'])
-@bot.message_handler(chat_types=['private'], content_types=['text'], commands=['start'])
+
+
+@bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, '1')
-
-
-@bot.message_handler(chat_types=['group'], content_types=['text'], commands=['hello'])
-def start(message):
-    bot.send_message(message.chat.id, '2')
-
-
-def create_start_btn():
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    btn1 = types.KeyboardButton("🐱кот")
-    btn2 = types.KeyboardButton("😀анекдот")
-    markup.add(btn1, btn2)
-    return markup
-
-
-@bot.message_handler(chat_types=['group'], content_types=['text'], commands=['start'])
-def send_welcome_message(message):
-    markup = create_start_btn()
-    bot.send_message(message.chat.id, f"Hi, I am TestBot", reply_markup=markup)
-
-
-@bot.message_handler(regexp=r'[0-9]')
-def start(message):
-    bot.send_message(message.chat.id, 'reg')
-
-
-@bot.message_handler(func=lambda x: True)
-def start(message):
-    bot.send_message(message.chat.id, 'all right')
+    mess1 = bot.send_message(message.chat.id, '_all_ right')
+    sleep(1)
+    bot.edit_message_text(chat_id=message.chat.id, message_id=mess1.id, text='ass')
 
 
 if __name__ == '__main__':
