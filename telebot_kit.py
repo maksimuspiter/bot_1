ffca3c3be10779d26d77cb91e76435eec3ca3fcf
maksimuspiter <maksimuspiter@gmail.com>
2023-03-15 13:38:35 +0300

    refactor start functions

--- telebot_kit.py
+++ telebot_kit.py
@@ -9,36 +9,50 @@ API_TOKEN = '5896308515:AAGZwJtgI1OZ_KlMNEzxWbjLT4v2KOlAlVs'
 bot = AsyncTeleBot(API_TOKEN)
 
 
-@bot.message_handler(commands=['help', 'start'])
-async def send_welcome(message):
+def create_start_btn():
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     btn1 = types.KeyboardButton("🐱кот")
     btn2 = types.KeyboardButton("😀анекдот")
     markup.add(btn1, btn2)
-    await bot.reply_to(message, "Hi there, I am EchoBot", reply_markup=markup)
+    return markup
 
 
-# Handle all other messages with content_type 'text' (content_types defaults to ['text'])
-@bot.message_handler(func=lambda message: True)
+def get_joke():
+    url = 'http://www.RzhuNeMogu.ru/Widzh/WidzhRNM.aspx?type=1&callback=onSuccess'
+    r = requests.get(url)
+
+    soup = BeautifulSoup(r.text, 'lxml')
+    soup = str(soup)
+    new = soup[(soup.index('{"result":') + 11): (soup.index('</p><p><a'))]
+    while '<br' in new:
+        new = new.replace('<br>', '')
+        new = new.replace('<br/>', '')
+    return new
+
+
+async def send_welcome_message(message):
+    markup = create_start_btn()
+    await bot.send_message(message.from_user.id,
+                           f"Hi {message.chat.first_name} , I am TestBot",
+                           reply_markup=markup)
+
+
+@bot.message_handler(commands=['help', 'start'])
+async def first(message):
+    await send_welcome_message(message)
+
+
+@bot.message_handler(content_types=['text'])
 async def send(message):
-    if message.text == "🐱кот":
-        img = requests.get('https://loremflickr.com/320/240', allow_redirects=True).url
-        # await bot.reply_to(message, img)
-        await bot.send_photo(message.from_user.id, img)
-    elif message.text == "😀анекдот":
-
-        url = 'http://www.RzhuNeMogu.ru/Widzh/WidzhRNM.aspx?type=1&callback=onSuccess'
-        r = requests.get(url)
-
-        soup = BeautifulSoup(r.text, 'lxml')
-        soup = str(soup)
-        print(soup)
-        new = soup[(soup.index('{"result":') + 11): (soup.index('</p><p><a'))]
-        while '<br' in new:
-            new = new.replace('<br>', '')
-            new = new.replace('<br/>', '')
-
-        await bot.send_message(message.from_user.id, new)
+    match message.text:
+        case "🐱кот":
+            img = requests.get('https://loremflickr.com/320/240', allow_redirects=True).url
+            await bot.send_photo(message.from_user.id, img)
+        case "😀анекдот":
+            joke = get_joke()
+            await bot.send_message(message.from_user.id, joke)
+        case _:
+            await send_welcome_message(message)
 
 
 if __name__ == '__main__':
