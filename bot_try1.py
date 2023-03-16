250da39b6c04ba1d589603592c4ce6066c02cfcf
maksimuspiter <maksimuspiter@gmail.com>
2023-03-16 11:30:04 +0300

    send message in channel

--- bot_try1.py
+++ bot_try1.py
@@ -7,16 +7,16 @@ from telebot import types
 bot = telebot.TeleBot('5896308515:AAGZwJtgI1OZ_KlMNEzxWbjLT4v2KOlAlVs')
 
 
-# @bot.message_handler(commands=['start'])
-
-
 @bot.message_handler(commands=['start'])
 def start(message):
-    kb = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
-    btn1 = types.KeyboardButton(text='Button 1')
-    btn2 = types.KeyboardButton(text='Button 2')
-    kb.add(btn1, btn2)
-    bot.send_message(message.chat.id, '1', reply_markup=kb)
+    bot.send_message(chat_id='@bot_test_stop', text='hello')
+    # https://t.me/+WihmswUzO_JjMjZi
+# @bot.callback_query_handler(func=lambda callback: callback.data)
+# def check_callback_data(callback):
+#     if callback.data == 'btn1':
+#         bot.send_message(callback.message.chat.id, 'You push btn1')
+#     elif callback.data == 'btn2':
+#         bot.send_message(callback.message.chat.id, 'You push btn2')
 
 
 if __name__ == '__main__':
