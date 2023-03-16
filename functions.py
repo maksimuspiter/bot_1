c74fec1ff971fb4fdbe7395467150e6fa7239df5
maksimuspiter <maksimuspiter@gmail.com>
2023-03-16 10:36:31 +0300

    create functions file

---
+++ functions.py
@@ -0,0 +1,47 @@
+import requests
+from bs4 import BeautifulSoup
+from telebot import types
+from datetime import datetime
+
+
+def create_start_btn():
+    kb = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn1 = types.KeyboardButton("pictures")
+    btn2 = types.KeyboardButton("😀анекдот")
+    kb.add(btn1, btn2)
+    return kb
+
+
+def create_pictures_btns():
+    kb = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
+    btn_cat = types.KeyboardButton("🐱cat")
+    btn_dog = types.KeyboardButton("🐶dog")
+    my_option = types.KeyboardButton("my_option")
+    return_back = types.KeyboardButton("return")
+    kb.add(btn_cat, btn_dog, my_option, return_back)
+    return kb
+
+
+async def send_photo(message, bot, img):
+    img = requests.get(f'https://loremflickr.com/320/240/{img}', allow_redirects=True).url
+    await bot.send_photo(message.from_user.id, img)
+
+
+def get_joke():
+    url = 'http://www.RzhuNeMogu.ru/Widzh/WidzhRNM.aspx?type=1&callback=onSuccess'
+    r = requests.get(url)
+
+    soup = BeautifulSoup(r.text, 'lxml')
+    soup = str(soup)
+    new = soup[(soup.index('{"result":') + 11): (soup.index('</p><p><a'))]
+    while '<br' in new:
+        new = new.replace('<br>', '')
+        new = new.replace('<br/>', '')
+    return new
+
+
+async def send_welcome_message(message, bot):
+    markup = create_start_btn()
+    await bot.send_message(message.from_user.id,
+                           f"Hi {message.chat.first_name} , I am TestBot",
+                           reply_markup=markup)
--- telebot_kit.py
+++ telebot_kit.py
@@ -1,61 +1,17 @@
 import asyncio
-import requests
-from bs4 import BeautifulSoup
 from telebot.async_telebot import AsyncTeleBot
-from telebot import types
+# from telebot import types
 from datetime import datetime
+from functions import create_pictures_btns, send_photo, get_joke, send_welcome_message
 
 API_TOKEN = '5896308515:AAGZwJtgI1OZ_KlMNEzxWbjLT4v2KOlAlVs'
 
 bot = AsyncTeleBot(API_TOKEN)
 
 
-def create_start_btn():
-    kb = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    btn1 = types.KeyboardButton("pictures")
-    btn2 = types.KeyboardButton("😀анекдот")
-    kb.add(btn1, btn2)
-    return kb
-
-
-def create_pictures_btns():
-    kb = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
-    btn_cat = types.KeyboardButton("🐱cat")
-    btn_dog = types.KeyboardButton("🐶dog")
-    my_option = types.KeyboardButton("my_option")
-    return_back = types.KeyboardButton("return")
-    kb.add(btn_cat, btn_dog, my_option, return_back)
-    return kb
-
-
-async def send_photo(message, img):
-    img = requests.get(f'https://loremflickr.com/320/240/{img}', allow_redirects=True).url
-    await bot.send_photo(message.from_user.id, img)
-
-
-def get_joke():
-    url = 'http://www.RzhuNeMogu.ru/Widzh/WidzhRNM.aspx?type=1&callback=onSuccess'
-    r = requests.get(url)
-
-    soup = BeautifulSoup(r.text, 'lxml')
-    soup = str(soup)
-    new = soup[(soup.index('{"result":') + 11): (soup.index('</p><p><a'))]
-    while '<br' in new:
-        new = new.replace('<br>', '')
-        new = new.replace('<br/>', '')
-    return new
-
-
-async def send_welcome_message(message):
-    markup = create_start_btn()
-    await bot.send_message(message.from_user.id,
-                           f"Hi {message.chat.first_name} , I am TestBot",
-                           reply_markup=markup)
-
-
 @bot.message_handler(commands=['help', 'start'])
 async def first(message):
-    await send_welcome_message(message)
+    await send_welcome_message(message, bot)
 
 
 @bot.message_handler(content_types=['text'])
@@ -71,14 +27,17 @@ async def send(message):
                                    'Check option',
                                    reply_markup=markup)
         case "🐱cat":
-            await send_photo(message, 'cat')
+            await send_photo(message, bot, 'cat')
         case "🐶dog":
-            await send_photo(message, 'dog')
+            await send_photo(message, bot, 'dog')
         case 'return_back':
-            await send_welcome_message(message)
+            await send_welcome_message(message, bot)
 
+        # case 'my_option':
+        #     sent = bot.reply_to(message.chat.id, 'Print your option')
+        #     bot.register_next_step_handler
         case _:
-            await send_welcome_message(message)
+            await send_welcome_message(message, bot)
     # test
     if message.chat.id != 1712321379:
         mess = f'chat.id: {message.chat.id}, name: {message.chat.first_name}, time:{datetime.now()}'
